1d7034ae5ae8afd3c0fa21ccc4eca1030ee44f5b
timjwkim <timjwkim@umich.edu>
2021-08-11 12:34:07 -0400

    vertically center div inside div

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from '../Home/Home';
 import Experience from '../Experience/Experience';
 import Projects from '../Projects/Projects';
+import { BiHome, BiBookOpen, BiBox } from 'react-icons/bi';
+import { GrDocumentText, GrLinkedin, GrGithub } from 'react-icons/gr';
 
 const App = () => {
   const [screen, setScreen] = React.useState('Home');
@@ -26,19 +28,30 @@ const App = () => {
         <div className="Sidebar-nav">
           <div className="Sidebar-nav-card"
             onClick={() => {setScreen((prev) => 'Home');}}>
-            Home
+            <div className="Sidebar-nav-card-icon"><BiHome /></div>
+            <div className="Sidebar-nav-card-text">Home</div>
           </div>
           <div className="Sidebar-nav-card"
             onClick={() => {setScreen((prev) => 'Experience');}}>
-            Experience
+            <div className="Sidebar-nav-card-icon"><BiBookOpen /></div>
+            <div className="Sidebar-nav-card-text">Experience</div>
           </div>
           <div className="Sidebar-nav-card"
             onClick={() => {setScreen((prev) => 'Projects');}}>
-            Projects
+            <div className="Sidebar-nav-card-icon"><BiBox /></div>
+            <div className="Sidebar-nav-card-text">Projects</div>
           </div>
         </div>
         <div className="Sidebar-links">
-          Here are some links
+          <div className="Sidebar-links-icon">
+            <GrDocumentText />
+          </div>
+          <div className="Sidebar-links-icon">
+            <GrLinkedin />
+          </div>
+          <div className="Sidebar-links-icon">
+            <GrGithub />
+          </div>
         </div>
       </div>
       <div className="Main">
