348f5176fbd29f4c153cf91ef6427e91b4f8984c
timjwkim <timjwkim@umich.edu>
2021-08-11 13:29:26 -0400

    implemented home content

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,7 +5,22 @@ const Home = () => {
 
   return (
     <div className="Home">
-      Home
+      <div className="Home-intro">
+        Hello!
+      </div>
+      <div className="Home-title">
+        I'm Tim.
+      </div>
+      <div className="Home-text">
+        Originally from the NYC area, I am a current senior studying Computer Science
+        at the University of Michigan. This past summer, I interned at Expedia Group
+        as a software development engineer. I am a hard-working team-player, eager
+        to learn and currently in search of a full-time position as a Software Engineer.
+      </div>
+      <div className="Home-buttons">
+        <button>Email Me!</button>
+        <button>View Resume</button>
+      </div>
     </div>
   );
 }
