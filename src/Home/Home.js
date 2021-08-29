f78f85561d88372ce93a7678b5161f2b5c987a49
timjwkim <timjwkim@umich.edu>
2021-08-29 17:14:03 -0400

    edit home page and change color scheme

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,14 +12,15 @@ const Home = () => {
         I'm Tim.
       </div>
       <div className="Home-text">
-        Originally from the NYC area, I am a current senior studying Computer Science
-        at the University of Michigan. This past summer, I interned at Expedia Group
-        as a software development engineer. I am a hard-working team-player, eager
-        to learn and currently in search of a full-time position as a Software Engineer.
+        Originally from the NYC area, I am a current senior studying <b>Computer
+        Science</b> at the <b>University of Michigan</b>. This past summer, I interned
+        at <b>Expedia Group</b> as a software development engineer. I am a hard-working 
+        team-player, eager to learn and currently in search of a full-time position
+        as a <b>Software Engineer</b>.
       </div>
       <div className="Home-buttons">
-        <button>Email Me!</button>
-        <button>View Resume</button>
+        <button className="Email">Email Me!</button>
+        <button className="Resume">View Resume</button>
       </div>
     </div>
   );
