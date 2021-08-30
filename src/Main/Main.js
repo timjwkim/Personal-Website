bb5629b6fecfc5bb6cc9796318a12f6c80e359da
timjwkim <timjwkim@umich.edu>
2021-08-30 11:56:40 -0400

    implemented main content

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main.css';
+import { IoReturnDownBack } from 'react-icons/io5';
 
 const Main = () => {
   const [screen, setScreen] = React.useState('Home');
@@ -9,15 +10,15 @@ const Main = () => {
       <div className="Main">
         <div className="Main-text">
           Originally from the NYC area, currently a senior studying computer science
-          at the <b onClick={() => {setScreen('UM')}}>University of Michigan</b>, looking for a full-time job as
-          a <b>Software Engineer</b>.
+          at the <b className="Main-link" onClick={() => {setScreen('UM')}}>University of Michigan</b>,
+          looking for a full-time job as a <b><i>Software Engineer</i></b>.
         </div>
         <div className="Main-sub">
           <div className="Main-sub-pres">
-            Currently part-time at UofM ITS
+            Currently part-time at <b className="Main-link" onClick={() => {setScreen('UMITS')}}>UofM ITS</b>.
           </div>
           <div className="Main-sub-prev">
-            Most recently at Expedia Group
+            Most recently at <b className="Main-link" onClick={() => {setScreen('EG')}}>Expedia Group</b>.
           </div>
         </div>
       </div>
@@ -28,10 +29,12 @@ const Main = () => {
     return (
       <div className="Main">
         <div className="Main-text">
-          University of Michigan
+          <b className="Main-link">University of Michigan</b> <span><i>(exp. graduation Dec 2021)</i></span>
         </div>
         <div className="Main-sub">
-          What i do <b onClick={() => {setScreen('Home')}}>back</b>
+          Relevant Coursework: Data Structures and Algorithms, Software
+          Engineering, Web Systems, Mobile App Development for Entrepreneurs, User
+          Interface Development, Introduction to Artificial Intelligence. <span><IoReturnDownBack onClick={() => {setScreen('Home')}}/></span>
         </div>
       </div>
     );
@@ -41,10 +44,12 @@ const Main = () => {
     return (
       <div className="Main">
         <div className="Main-text">
-          University of Michigan ITS
+          <b className="Main-link">University of Michigan ITS</b> <span><i>(Nov. 2019 - Present)</i></span>
         </div>
         <div className="Main-sub">
-          What i do <b onClick={() => {setScreen('Home')}}>back</b>
+          Provide technical support to customers and perform maintenance to network
+          devices and other technical equipment deployed at the university, troubleshooting
+          any hardware and software problems. <span><IoReturnDownBack onClick={() => {setScreen('Home')}}/></span>
         </div>
       </div>
     );
@@ -54,10 +59,12 @@ const Main = () => {
     return (
       <div className="Main">
         <div className="Main-text">
-          Expedia Group
+          <b className="Main-link">Expedia Group</b> <span><i>(Summer 2021)</i></span>
         </div>
         <div className="Main-sub">
-          What i do <b onClick={() => {setScreen('Home')}}>back</b>
+          Built the <b><i>Datahub Health Dashboard</i></b> using React, ES, and
+          GraphQL and supported the Datahub team by handling feature implementation,
+          UI design change, and minor bug fix tickets. <span><IoReturnDownBack onClick={() => {setScreen('Home')}}/></span>
         </div>
       </div>
     );
